8da67d45878fad90255df975924760898038e9f4
fengzi <2798776062@qq.com>
2026-07-09 09:57:39 +0800

    项目初始化

---
+++ salary-manage-system/src/main/java/com/heima/salarymanagesystem/SalaryManageSystemApplication.java
@@ -0,0 +1,14 @@
+package com.heima.salarymanagesystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalaryManageSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SalaryManageSystemApplication.class, args);
+        
+    }
+
+}
---
+++ salary-manage-system/src/test/java/com/heima/salarymanagesystem/SalaryManageSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heima.salarymanagesystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalaryManageSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
